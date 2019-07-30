3e0a5d647a61f6ece3c30edf6e9cd6774ba48c9f
Ralf Ebert <info@ralfebert.de>
2019-07-30 18:07:39 +0200

    example project

--- OperationQueueExample/Controller/BackgroundOperationViewController.swift
+++ OperationQueueExample/Controller/BackgroundOperationViewController.swift
@@ -5,28 +5,13 @@ import UIKit
 
 class BackgroundOperationViewController: UIViewController {
 
-    let backgroundQueue = OperationQueue()
-
     @IBOutlet var resultsLabel: UILabel!
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
 
     @IBAction func startCalculate() {
 
-        self.activityIndicator.isHidden = false
-        self.calculateInBackground { result in
+        self.resultsLabel.text = String(approximatePi())
 
-            OperationQueue.main.addOperation {
-                self.resultsLabel.text = String(result)
-                self.activityIndicator.isHidden = true
-            }
-        }
-
-    }
-
-    func calculateInBackground(completion: @escaping (_ result: Double) -> Void) {
-        self.backgroundQueue.addOperation {
-            completion(approximatePi())
-        }
     }
 
 }
