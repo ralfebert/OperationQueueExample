1e0cf0fb5c9bb5aa845145b58ac183d050308bf4
Ralf Ebert <info@ralfebert.de>
2019-07-30 17:55:06 +0200

    Project setup OperationQueueExample

---
+++ OperationQueueExample/App/OperationQueueExampleAppDelegate.swift
@@ -0,0 +1,14 @@
+// © 2019 Ralf Ebert — OperationQueueExample
+
+import UIKit
+
+@UIApplicationMain
+class OperationQueueExampleAppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
